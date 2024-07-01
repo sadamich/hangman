4e06bb9c9c0130f4ba4eba6d2094ba424021ff6a
unknown <sadamichikato@gmail.com>
2024-07-01 09:45:56 +0200

    nls introduction

---
+++ nls_introduction.py
@@ -0,0 +1,7 @@
+
+
+ott_Toomet= print("Non-linear optimization is like climbing uphill in whiteout conditions where
+you cannot distinguish any details around you–sky is just a white fog and the
+ground is covered with similar white snow.")
+
+
