104d200a61941ad392c09b8e1a44f110bf56f02e
unknown <sadamichikato@gmail.com>
2024-07-06 16:41:01 +0200

    eine neue Zeile

--- nls_introduction.py
+++ nls_introduction.py
@@ -1,7 +1,16 @@
 
 
-ott_Toomet= print("Non-linear optimization is like climbing uphill in whiteout conditions where
+ott_Toomet= print("""Non-linear optimization is like climbing uphill in whiteout conditions where
 you cannot distinguish any details around you–sky is just a white fog and the
-ground is covered with similar white snow.")
+ground is covered with similar white snow.""")
 
+
+
+
+
+###self.taught  p.20   (""" , """) ###
+
+
+
+###https://cran.r-project.org/web/packages/maxLik/vignettes/intro-to-maximum-likelihood.pdf   p.13 ###
 
