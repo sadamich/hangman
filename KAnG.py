e4f06d5f550816c527714a496e26074216ce9481
unknown <sadamichikato@gmail.com>
2024-07-06 16:38:20 +0200

    KAnG 3

---
+++ KAnG.py
@@ -0,0 +1,12 @@
+KAnG = print("Bundes-Klimaanpassungsgesetz")
+
+
+KAnG §3 = print("Vorsorgende Klimaanpassungsstrategie")
+
+KAnG §3(1) = print("""Die  Bundesregierung  legt  bis  zum  Ablauf  des  30.
+September  2025eine  vorsorgende Klimaanpassungsstrategie  mit  messbaren
+Zielen  vor.  Sie  setzt  sie  im Rahmen  ihrer  Zuständigkeit  um  und
+schreibt  sie  unter  Berücksichtigung  aktueller  wissenschaftlicher
+Erkenntnisse  alle  vier  Jahre  fort.  Die Klimaanpassungsstrategie wird
+insbesondere auf Grundlage der Klimarisikoanalyse nach § 4 Absatz 1 Satz 1
+entwickelt""")
