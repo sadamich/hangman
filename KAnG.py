7ff085a96048c9d8e57f077b358ddc1c7e37b120
unknown <sadamichikato@gmail.com>
2024-07-07 16:46:04 +0200

    neue Zeilen

--- KAnG.py
+++ KAnG.py
@@ -10,3 +10,63 @@ schreibt  sie  unter  Berücksichtigung  aktueller  wissenschaftlicher
 Erkenntnisse  alle  vier  Jahre  fort.  Die Klimaanpassungsstrategie wird
 insbesondere auf Grundlage der Klimarisikoanalyse nach § 4 Absatz 1 Satz 1
 entwickelt""")
+
+
+KAnG §3(2) = print(""" In  die  vorsorgende  Klimaanpassungsstrategie  sind
+mindestens  folgende  Cluster  und  ihnen  zugeordnete 
+Handlungsfelder aufzunehmen:""")
+
+KAnG §3(2) 1 = print("""  1. das Cluster Infrastruktur mit folgenden Handlungsfeldern:
+
+a) Energieinfrastruktur,
+b) Gebäude und
+c) Verkehr und Verkehrsinfrastruktur,""")
+
+KAnG §3(2) 2 = print(""" das Cluster Land und Landnutzung mit folgenden Handlungsfeldern:
+
+a) biologische Vielfalt,
+b) Boden,
+c) Landwirtschaft und
+d) Wald und Forstwirtschaft,""")
+
+KAnG §3(2) 3 = print(" das Cluster menschliche Gesundheit und Pflege,")
+KAnG §3(2) 4 = print(""" das Cluster Stadtentwicklung, Raumplanung und Bevölkerungsschutz mit folgenden Handlungsfeldern:
+
+a) Bevölkerungs- und Katastrophenschutz,
+b) Raumplanung und
+c) Stadt- und Siedlungsentwicklung,""")
+
+KAnG §3(2)5 = print("""das Cluster Wasser mit folgenden Handlungsfeldern:
+
+a) Fischerei,
+b) Küsten- und Meeresschutz und
+c) Wasserhaushalt  und  Wasserwirtschaft,  einschließlich  Hoch-  und  Niedrigwasserrisikomanagement  sowie 
+Starkregenrisikomanagement,""")
+
+KAnG §3(2)6 = print(""" das Cluster Wirtschaft mit folgenden Handlungsfeldern:
+a) Finanzwirtschaft und
+b) Industrie und Gewerbe sowie
+7. ein Cluster mit übergreifenden Handlungsfeldern, wie beispielsweise vulnerable Gruppen oder Arbeitsschutz.""")
+
+KAnG §3(3) = print(" Die vorsorgende Klimaanpassungsstrategie")
+
+KAnG §3(3)1 = print("""enthält  hinreichend  ambitionierte,  messbare  Ziele,  die  jeweils  innerhalb  eines  bestimmten  in  der  Strategie 
+festzulegenden  zeitlichen  Rahmens  erreicht  werden  sollen  und  einem  Cluster  zugeordnet  sind;  diese  Ziele 
+
+konkretisieren das übergeordnete Ziel nach § 1,
+2. definiert für jedes Ziel einen oder mehrere Indikatoren, mit denen gemessen wird, inwieweit das Ziel erreicht 
+worden ist,
+3. benennt geeignete Maßnahmen des Bundes, die jeweils zur Erreichung eines oder mehrerer Ziele beitragen,
+4. gibt Empfehlungen zu Maßnahmen, die in die Zuständigkeit der Länder fallen, und
+5. legt einen Mechanismus zur Bewertung der Fortschritte in der Zielerreichung fest.
+
+Im Rahmen der Benennung von Maßnahmen nach Satz 1 Nummer 3 und der Empfehlung von Maßnahmen nach 
+Satz  1  Nummer  4  sollen  im  Fall  des  Vorliegens  mehrerer  gleich  geeigneter  Maßnahmen  nachhaltige 
+Anpassungsmaßnahmen Vorrang haben, insbesondere solche, die ausgeprägte Synergien zu den Bereichen 
+des natürlichen Klimaschutzes, des Schutzes der biologischen Vielfalt, des resilienten Wasserhaushalts, der 
+blau-grünen Infrastruktur oder der nachhaltigen Stadt- und Siedlungsentwicklung aufweisen.""")
+
+KAnG §3(4) = print(""" Die  Länder,  Verbände  und  die  Öffentlichkeit  sind  bei  der  Festlegung  von  messbaren  Zielen  und  den
+
+entsprechenden Indikatoren sowie bei der Auswahl von Maßnahmen zu beteiligen.""")
+
