4eb37d45d6c70dd6d6c3898eff2ab51a01e8b2d9
unknown <sadamichikato@gmail.com>
2024-07-15 14:14:44 +0200

    stringmanipulation

---
+++ string_manupulation.py
@@ -0,0 +1,57 @@
+
+Liebling = "Taylor Swift"
+print(Liebling[0])
+print(Liebling[1])
+print(Liebling[2])
+print(Liebling[3])
+print(Liebling[4])
+print(Liebling[5])
+print(Liebling[6])
+print(Liebling[7])
+print(Liebling[8])
+print(Liebling[9])
+print(Liebling[10])
+print(Liebling[11])
+
+
+who = "you"
+what = "a letter"
+print("I wrote {} {}.".format(who, what))
+
+
+print("Das ist die Liebe.".upper())
+
+
+
+
+print("who, where, what".split(","))
+
+
+
+Worte = ["Das", "ist", "die", "Liebe", "."]
+print("".join(Worte))
+print(" ".join(Worte))
+
+
+taylor = "swift"
+taylor = taylor.replace("i", "%")
+print(taylor)
+
+
+print("taylor".index("y"))
+
+
+
+print("She said, 'you are right'.")
+
+
+
+print("Liebe"+ "Liebe"+ "Liebe")
+print("Liebe"*3)
+
+
+
+Liebe = "Ich mag taylor"
+slice = Liebe[0:5]
+print(slice)
+
