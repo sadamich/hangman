236a7181ec29d048b812c54143cb1d7d6c62a7ab
unknown <sadamichikato@gmail.com>
2024-07-15 14:17:14 +0200

    ifelse

---
+++ if_else_elif_age.py
@@ -0,0 +1,30 @@
+
+
+age = 30
+if age > 25:
+    print("You are old.")
+elif 10 <= age <= 19:
+    print("You are teenager.")
+else:
+    print("You are stars.")
+
+    
+
+
+age = 15
+if age > 25:
+    print("You are old.")
+elif 10 <= age <= 19:
+    print("You are teenager.")
+    
+else:
+    print("You are stars.")
+
+
+age = 9
+if age > 25:
+    print("You are old.")
+elif 10<= age <= 19:
+    print("You are teenager.")
+else:
+    print("You are stars.")
