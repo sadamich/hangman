777b4740546fe2f53dedc1c269b28dd592f93983
unknown <sadamichikato@gmail.com>
2024-07-15 13:54:54 +0200

    neue Zeilen

--- KAnG.py
+++ KAnG.py
@@ -1,72 +1,67 @@
 KAnG = print("Bundes-Klimaanpassungsgesetz")
 
 
-KAnG §3 = print("Vorsorgende Klimaanpassungsstrategie")
-
-KAnG §3(1) = print("""Die  Bundesregierung  legt  bis  zum  Ablauf  des  30.
-September  2025eine  vorsorgende Klimaanpassungsstrategie  mit  messbaren
-Zielen  vor.  Sie  setzt  sie  im Rahmen  ihrer  Zuständigkeit  um  und
-schreibt  sie  unter  Berücksichtigung  aktueller  wissenschaftlicher
-Erkenntnisse  alle  vier  Jahre  fort.  Die Klimaanpassungsstrategie wird
-insbesondere auf Grundlage der Klimarisikoanalyse nach § 4 Absatz 1 Satz 1
-entwickelt""")
+KAnG_3 = print("Vorsorgende Klimaanpassungsstrategie")
 
 
-KAnG §3(2) = print(""" In  die  vorsorgende  Klimaanpassungsstrategie  sind
-mindestens  folgende  Cluster  und  ihnen  zugeordnete 
-Handlungsfelder aufzunehmen:""")
+KAnG_3_1 = print("Die  Bundesregierung  legt  bis  zum  Ablauf  des  30. September  2025eine  vorsorgende Klimaanpassungsstrategie  mit  messbaren Zielen  vor.")
 
-KAnG §3(2) 1 = print("""  1. das Cluster Infrastruktur mit folgenden Handlungsfeldern:
+KAnG_3_1 = print("Sie  setzt  sie  im Rahmen  ihrer  Zuständigkeit  um  und schreibt  sie  unter  Berücksichtigung  aktueller  wissenschaftlicher Erkenntnisse  alle  vier  Jahre  fort.")
 
-a) Energieinfrastruktur,
-b) Gebäude und
-c) Verkehr und Verkehrsinfrastruktur,""")
+KAnG_3_1 = print("Die Klimaanpassungsstrategie wird insbesondere auf Grundlage der Klimarisikoanalyse nach § 4 Absatz 1 Satz 1entwickelt")
 
-KAnG §3(2) 2 = print(""" das Cluster Land und Landnutzung mit folgenden Handlungsfeldern:
 
-a) biologische Vielfalt,
-b) Boden,
-c) Landwirtschaft und
-d) Wald und Forstwirtschaft,""")
+KAnG_3_2 = print(" In  die  vorsorgende  Klimaanpassungsstrategie  sind mindestens  folgende  Cluster  und  ihnen  zugeordnete  Handlungsfelder aufzunehmen:")
 
-KAnG §3(2) 3 = print(" das Cluster menschliche Gesundheit und Pflege,")
-KAnG §3(2) 4 = print(""" das Cluster Stadtentwicklung, Raumplanung und Bevölkerungsschutz mit folgenden Handlungsfeldern:
+KAnG_3_2_1 = print("1. das Cluster Infrastruktur mit folgenden Handlungsfeldern:")
+                 
+KAnG_3_2_1 = print(" a) Energieinfrastruktur, b) Gebäude und c) Verkehr und Verkehrsinfrastruktur,")
 
-a) Bevölkerungs- und Katastrophenschutz,
-b) Raumplanung und
-c) Stadt- und Siedlungsentwicklung,""")
+KAnG_3_2_2 = print("das Cluster Land und Landnutzung mit folgenden Handlungsfeldern: a) biologische Vielfalt, b) Boden, c) Landwirtschaft und d) Wald und Forstwirtschaft,")
 
-KAnG §3(2)5 = print("""das Cluster Wasser mit folgenden Handlungsfeldern:
+KAnG_3_2_3 = print(" das Cluster menschliche Gesundheit und Pflege,")
+                 
+KAnG_3_2_4 = print(" das Cluster Stadtentwicklung, Raumplanung und Bevölkerungsschutz mit folgenden Handlungsfeldern:")
+                    
 
-a) Fischerei,
-b) Küsten- und Meeresschutz und
-c) Wasserhaushalt  und  Wasserwirtschaft,  einschließlich  Hoch-  und  Niedrigwasserrisikomanagement  sowie 
-Starkregenrisikomanagement,""")
+KAnG_3_2_4 = print(" a) Bevölkerungs- und Katastrophenschutz, b) Raumplanung und c) Stadt- und Siedlungsentwicklung,")
 
-KAnG §3(2)6 = print(""" das Cluster Wirtschaft mit folgenden Handlungsfeldern:
-a) Finanzwirtschaft und
-b) Industrie und Gewerbe sowie
-7. ein Cluster mit übergreifenden Handlungsfeldern, wie beispielsweise vulnerable Gruppen oder Arbeitsschutz.""")
+KAnG_3_2_5 = print("das Cluster Wasser mit folgenden Handlungsfeldern:")
 
-KAnG §3(3) = print(" Die vorsorgende Klimaanpassungsstrategie")
+KAnG_3_2_5 = print("a) Fischerei, b) Küsten- und Meeresschutz und c) Wasserhaushalt  und  Wasserwirtschaft,  einschließlich  Hoch-  und  Niedrigwasserrisikomanagement  sowie Starkregenrisikomanagement,")
 
-KAnG §3(3)1 = print("""enthält  hinreichend  ambitionierte,  messbare  Ziele,  die  jeweils  innerhalb  eines  bestimmten  in  der  Strategie 
-festzulegenden  zeitlichen  Rahmens  erreicht  werden  sollen  und  einem  Cluster  zugeordnet  sind;  diese  Ziele 
+KAnG_3_2_6 = print("das Cluster Wirtschaft mit folgenden Handlungsfeldern:")
+                 
+KAnG_3_2_6 = print("a) Finanzwirtschaft und b) Industrie und Gewerbe sowie")
 
-konkretisieren das übergeordnete Ziel nach § 1,
-2. definiert für jedes Ziel einen oder mehrere Indikatoren, mit denen gemessen wird, inwieweit das Ziel erreicht 
-worden ist,
-3. benennt geeignete Maßnahmen des Bundes, die jeweils zur Erreichung eines oder mehrerer Ziele beitragen,
-4. gibt Empfehlungen zu Maßnahmen, die in die Zuständigkeit der Länder fallen, und
-5. legt einen Mechanismus zur Bewertung der Fortschritte in der Zielerreichung fest.
+KAnG_3_2_7 = print(" ein Cluster mit übergreifenden Handlungsfeldern, wie beispielsweise vulnerable Gruppen oder Arbeitsschutz.")
 
-Im Rahmen der Benennung von Maßnahmen nach Satz 1 Nummer 3 und der Empfehlung von Maßnahmen nach 
-Satz  1  Nummer  4  sollen  im  Fall  des  Vorliegens  mehrerer  gleich  geeigneter  Maßnahmen  nachhaltige 
-Anpassungsmaßnahmen Vorrang haben, insbesondere solche, die ausgeprägte Synergien zu den Bereichen 
-des natürlichen Klimaschutzes, des Schutzes der biologischen Vielfalt, des resilienten Wasserhaushalts, der 
-blau-grünen Infrastruktur oder der nachhaltigen Stadt- und Siedlungsentwicklung aufweisen.""")
+KAnG_3_3 = print(" Die vorsorgende Klimaanpassungsstrategie")
 
-KAnG §3(4) = print(""" Die  Länder,  Verbände  und  die  Öffentlichkeit  sind  bei  der  Festlegung  von  messbaren  Zielen  und  den
+KAnG_3_3_1 = print("enthält  hinreichend  ambitionierte,  messbare  Ziele,  die  jeweils  innerhalb  eines  bestimmten  in  der  Strategie festzulegenden  zeitlichen  Rahmens  erreicht  werden  sollen  und  einem  Cluster  zugeordnet  sind;")
 
-entsprechenden Indikatoren sowie bei der Auswahl von Maßnahmen zu beteiligen.""")
+AKnG_3_3_1 = print("diese  Ziele konkretisieren das übergeordnete Ziel nach § 1,")
+
+
+KAnG_3_2 = print("definiert für jedes Ziel einen oder mehrere Indikatoren, mit denen gemessen wird, inwieweit das Ziel erreicht worden ist,")
+
+KAnG_3_3 = print(" benennt geeignete Maßnahmen des Bundes, die jeweils zur Erreichung eines oder mehrerer Ziele beitragen,")
+
+KAnG_3_4 = print("gibt Empfehlungen zu Maßnahmen, die in die Zuständigkeit der Länder fallen, und")
+
+KAnG_3_5 = print("legt einen Mechanismus zur Bewertung der Fortschritte in der Zielerreichung fest.")
+
+KAnG_3_5 = print("Im Rahmen der Benennung von Maßnahmen nach Satz 1 Nummer 3 und der Empfehlung von Maßnahmen nach Satz  1  Nummer  4  sollen  im  Fall  des  Vorliegens  mehrerer  gleich  geeigneter  Maßnahmen  nachhaltige Anpassungsmaßnahmen Vorrang haben,")
+
+KAnG_3_5 = print("insbesondere solche, die ausgeprägte Synergien zu den Bereichen des natürlichen Klimaschutzes, des Schutzes der biologischen Vielfalt, des resilienten Wasserhaushalts, der blau-grünen Infrastruktur oder der nachhaltigen Stadt- und Siedlungsentwicklung aufweisen.")
+
+KAnG_4 = print(" Die  Länder,  Verbände  und  die  Öffentlichkeit  sind  bei  der  Festlegung  von  messbaren  Zielen  und  den entsprechenden Indikatoren sowie bei der Auswahl von Maßnahmen zu beteiligen.")
+
+
+KAnG_5 = print("Verantwortlich für die Aufstellung, Überprüfung und gegebenenfalls Aktualisierung der Ziele nach Absatz 3 Satz 1 Nummer 1")
+
+KAnG_5 = print("sowie für die Benennung, Umsetzung und gegebenenfalls Aktualisierung der Maßnahmen nach Absatz 3 Satz 1 Nummer 3 im Rahmen der Aufstellung und der Fortschreibung der vorsorgenden Klimaanpassungsstrategie nach Absatz 1 ist das jeweils aufgrund seines Geschäftsbereichs für ein Ziel")
+
+KAnG_5 = print(" oder eine Maßnahme fachlich überwiegend zuständige Bundesministerium in Abstimmung mit den ebenfalls fachlich betroffenen Bundesministerien. Die Zuständigkeitsverteilung innerhalb der Bundesregierung bleibt unberührt.")
+
 
