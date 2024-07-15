133ac061c67f2fa733b14b466ebb686bf1cc7149
unknown <sadamichikato@gmail.com>
2024-07-15 14:19:38 +0200

    sq_str_mult_div

---
+++ func_sq_str_mult_div.py
@@ -0,0 +1,33 @@
+def squared(x):
+    return x**2
+print(squared(2))
+
+
+
+
+def print_string(string):
+    print(string)
+print_string("Testing:1. 2. 3")
+
+
+
+
+
+def add_mult(a, b, c, d= 100, e = 100):
+    return  (a + b+ c)* d/e
+result = add_mult(1, 2, 3)
+print(result)
+
+
+
+
+def divide(f):
+    return f/2
+
+def multiply(f):
+    return f * 4
+
+
+g= divide(4)
+h = multiply(g)
+print(h)
